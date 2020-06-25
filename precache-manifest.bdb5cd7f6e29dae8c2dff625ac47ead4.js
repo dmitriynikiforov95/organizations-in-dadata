ffd23c15fd7c2091195222f6c195cbc283c68a08
Dmitriy Nikiforov <nikiforov.psy@gmail.com>
2020-06-25 19:38:47 +0300

    Updates

---
+++ precache-manifest.bdb5cd7f6e29dae8c2dff625ac47ead4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07d94814e46442522cc624f3348eaf3",
+    "url": "/organizations-in-dadata/index.html"
+  },
+  {
+    "revision": "2e8ef7b42d9e29f20f4e",
+    "url": "/organizations-in-dadata/static/css/main.833481b7.chunk.css"
+  },
+  {
+    "revision": "f86bffb68e4ae369e7ca",
+    "url": "/organizations-in-dadata/static/js/2.3298a178.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/organizations-in-dadata/static/js/2.3298a178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8ef7b42d9e29f20f4e",
+    "url": "/organizations-in-dadata/static/js/main.3c5327d9.chunk.js"
+  },
+  {
+    "revision": "e68e443e0d920784ce69",
+    "url": "/organizations-in-dadata/static/js/runtime-main.7193ffc3.js"
+  },
+  {
+    "revision": "8587c4e495d2655c333291f211d15ab8",
+    "url": "/organizations-in-dadata/static/media/app-logo.8587c4e4.svg"
+  },
+  {
+    "revision": "c1ae67a7f017170e3f2733b81a18f635",
+    "url": "/organizations-in-dadata/static/media/trash-can.c1ae67a7.svg"
+  }
+]);
